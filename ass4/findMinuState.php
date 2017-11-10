564a7c34f186dd77d8dc4f668afc7ea8292844a2
Alajaji <Abdullahaj93@gmail.com>
2017-11-09 19:33:02 -0800

    Update findMinuState.php

--- ass4/findMinuState.php
+++ ass4/findMinuState.php
@@ -56,23 +56,6 @@ print $query;
 Result of query:
 <p>
 
-<?php
-$result = mysqli_query($conn, $query)
-or die(mysqli_error($conn));
-
-print "<pre>";
-while($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-  {
-    print "\n";
-    print "$row[s.description] $row[total_spent]";
-  }
-print "</pre>";
-
-mysqli_free_result($result);
-
-mysqli_close($conn);
-
-?>
 
 <p>
 <hr>
