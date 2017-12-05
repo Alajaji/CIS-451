bc9e651c11532df2e0f1838dd7c06b25009d2db1
Alajaji <Abdullahaj93@gmail.com>
2017-12-04 16:12:02 -0800

    Add files via upload

---
+++ FinalProject/team.php
@@ -0,0 +1,87 @@
+<?php
+
+include('connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die('Error connecting to MySQL server.');
+
+?>
+
+<html>
+<head>
+  <title>Another Simple PHP-MySQL Program</title>
+  </head>
+  
+  <body bgcolor="white">
+  
+  
+  <hr>
+  
+  
+<?php
+  
+$team = $_POST['team'];
+
+$team = mysqli_real_escape_string($conn, $team);
+// this is a small attempt to avoid SQL injection
+// better to use prepared statements
+
+$query = "SELECT 
+    p.name, pos.position
+FROM
+    (Player p, Player_has_Position pp, Position pos)
+    JOIN Team t ON (t.id = p.Team_id)
+    AND (pos.pos_code = pp.Position_pos_code)
+    AND (p.name = pp.Player_name)
+    WHERE t.name LIKE '%".$team."%'";
+
+?>
+
+<p>
+The query:
+<p>
+<?php
+print $query;
+?>
+
+<hr>
+<p>
+print $team:
+<p>
+
+<?php
+$result = mysqli_query($conn, $query)
+or die(mysqli_error($conn));
+
+print "<pre>";
+print " ==========================";
+print "\n";
+print " Players";
+print "\n";
+print " ==========================";
+print "\n";
+
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH))  
+  {
+    print "\n";
+    print " -------------------------";
+    print "$row[Player] | $row[Position]";
+  }
+print "</pre>";
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+?>
+
+<p>
+<hr>
+
+<p>
+<a href="team.txt" >Contents</a>
+of the PHP program that created this page. 	 
+ 
+</body>
+</html>
+	  
